eccc6f388f455d9c3f85fb11ff11a92d44940632
govindpvenu <govindpvenu.txt@gmail.com>
2025-12-31 11:21:37 +0530

    feat: setup PWA with sewist

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,56 @@
-import type { Metadata } from "next";
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "@/components/providers";
+import type { Metadata, Viewport } from "next";
+
+const APP_NAME = "Just Between Us";
+const APP_DEFAULT_TITLE = "Just Between Us";
+const APP_TITLE_TEMPLATE = "%s - Just Between Us";
+const APP_DESCRIPTION = "Shhh...it's just between us.";
+
+export const metadata: Metadata = {
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#FFFFFF",
+};
 
 const jetbrainsMono = JetBrains_Mono({
   variable: "--font-jetbrains-mono",
   subsets: ["latin"],
 });
-
-export const metadata: Metadata = {
-  title: "Just Between Us",
-  description: "Shhh...it's just between us.",
-};
 
 export default function RootLayout({
   children,
@@ -19,7 +58,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" dir="ltr">
       <body className={`${jetbrainsMono.variable} antialiased`}>
         <Providers>{children}</Providers>
       </body>
--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -3,13 +3,18 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RealtimeProvider } from "@upstash/realtime/client";
 import { useState } from "react";
+import { SerwistProvider } from "@serwist/turbopack/react";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <RealtimeProvider>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </RealtimeProvider>
+    <SerwistProvider swUrl="/sw.ts">
+      <RealtimeProvider>
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      </RealtimeProvider>
+    </SerwistProvider>
   );
 };
